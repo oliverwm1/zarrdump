ad43efa1d0809d7eb187a0652905392faeae5d2c
Oliver Watt-Meyer <oliverwatt@gmail.com>
2020-11-18 22:59:51 -0500

    Add ability to dump info for non-xarray zarrs

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,30 +1,46 @@
 import zarrdump
-from zarrdump.core import _open_mapper, dump
+from zarrdump.core import dump, _open_zarr 
 
 from click.testing import CliRunner
 import fsspec
 import pytest
+import numpy as np
 import xarray as xr
+import zarr
 
 
 def test_version():
     assert zarrdump.__version__ == '0.1.1'
 
 @pytest.fixture()
-def tmpzarr(tmpdir):
-    def to_zarr(consolidated=False):
+def tmp_xarray_ds(tmpdir):
+    def write_ds_to_zarr(consolidated=False):
         ds = xr.Dataset({"var1": xr.DataArray(range(3))})
         path = str(tmpdir.join("test.zarr"))
         ds.to_zarr(path, consolidated=consolidated)
         return ds, path
-    return to_zarr
+    return write_ds_to_zarr
+
+
+@pytest.fixture()
+def tmp_zarr_group(tmpdir):
+    def write_group_to_zarr(consolidated=False):
+        path = str(tmpdir.join("test.zarr"))
+        z = zarr.open_group(path)
+        arr = z.create_dataset("var1", shape=(3, 5))
+        arr[:] = 1.0
+        if consolidated:
+            zarr.consolidate_metadata(path)
+        return z, path
+    return write_group_to_zarr
 
 
 @pytest.mark.parametrize("consolidated", [True, False])
-def test__open_mapper(tmpzarr, consolidated):
-    ds, path = tmpzarr(consolidated=consolidated)
-    ds2 = _open_mapper(fsspec.get_mapper(path))
-    xr.testing.assert_identical(ds, ds2)
+def test__open_zarr(tmp_zarr_group, consolidated):
+    group, path = tmp_zarr_group(consolidated=consolidated)
+    opened_group, opened_group_is_consolidated = _open_zarr(fsspec.get_mapper(path))
+    np.testing.assert_allclose(group["var1"], opened_group["var1"])
+    assert consolidated == opened_group_is_consolidated
 
 
 def test_dump_non_existent_url():
@@ -35,8 +51,16 @@ def test_dump_non_existent_url():
 
 
 @pytest.mark.parametrize("options", [[], ["-v", "var1"]])
-def test_dump_executes(tmpzarr, options):
+def test_dump_executes_on_zarr_group(tmp_zarr_group, options):
     runner = CliRunner()
-    ds, path = tmpzarr()
+    _, path = tmp_zarr_group()
+    result = runner.invoke(dump, [path] + options)
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize("options", [[], ["-v", "var1"]])
+def test_dump_executes_on_xarray_dataset(tmp_xarray_ds, options):
+    runner = CliRunner()
+    _, path = tmp_xarray_ds()
     result = runner.invoke(dump, [path] + options)
     assert result.exit_code == 0
--- zarrdump/core.py
+++ zarrdump/core.py
@@ -13,30 +13,36 @@ def dump(url: str, variable: str):
     fs, _, _ = fsspec.get_fs_token_paths(url)
     if not fs.exists(url):
         raise click.ClickException(f"No file or directory at {url}")
+
     m = fs.get_mapper(url)
-    printme, is_xarray_dataset = _open_mapper(m)
+    zarr_obj, consolidated = _open_zarr(m)
+
+    if _zarr_object_is_xarray_dataset(zarr_obj):
+        printme = xr.open_zarr(m, consolidated=consolidated)
+        object_is_xarray = True
+    else:
+        printme = zarr_obj
+        object_is_xarray = False
+
     if variable is not None:
         printme = printme[variable]
-    if is_xarray_dataset:
-        print(printme)
-    else:
-        print(printme.info)
+
+    if not object_is_xarray:
+        printme = printme.info
+
+    print(printme)
 
 
-def _open_mapper(m: fsspec.FSMap) -> Union[xr.Dataset, zarr.hierarchy.Group, zarr.core.Array]:
+def _open_zarr(m: fsspec.FSMap) -> Union[zarr.hierarchy.Group, zarr.core.Array]:
     try:
         result = zarr.open_consolidated(m)
         consolidated=True
     except KeyError:
+        # un-consolidated group, or array
         result = zarr.open(m)
         consolidated=False
 
-    is_xarray_dataset = _zarr_object_is_xarray_dataset(result)
-
-    if is_xarray_dataset:
-        result = xr.open_zarr(m, consolidated=consolidated)
-    
-    return result, is_xarray_dataset
+    return result, consolidated
 
 
 def _zarr_object_is_xarray_dataset(
