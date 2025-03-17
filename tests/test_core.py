f3316f41c0a0563853e4bc4b90afff9115f77b0d
garciam <garciam@predictia.es>
2025-03-17 13:04:11 +0100

    add support for storage_options

--- tests/test_core.py
+++ tests/test_core.py
@@ -114,3 +114,22 @@ def test_dump_max_rows_limited(tmp_xarray_ds):
     _, path = tmp_xarray_ds(consolidated=True, n_vars=30)
     result = runner.invoke(dump, [path, "-m", 10])
     assert len(result.output.split("\n")) < 20  # give some buffer over 10
+
+
+@pytest.mark.parametrize("token", ["anon", "invalid_test_token"])
+def test_storage_options(token):
+    runner = CliRunner()
+    result = runner.invoke(
+        dump,
+        [
+            "--storage-option",
+            f"token={token}",
+            "gs://gcp-public-data-arco-era5/co/single-level-reanalysis.zarr",
+        ]
+    )
+    if token == "anon":
+        assert result.exit_code == 0
+        assert len(result.output.split("\n")) > 30
+    else:
+        assert result.exit_code == 1
+        assert str(result.exception) == "Invalid Credentials, 401"
--- zarrdump/core.py
+++ zarrdump/core.py
@@ -1,11 +1,28 @@
 from typing import Tuple, Union
 
-import click
 import fsspec
+import click
 import xarray as xr
 import zarr
 
 ZARR_MAJOR_VERSION = zarr.__version__.split(".")[0]
+
+
+# From https://stackoverflow.com/questions/51164033/python-click-multiple-key-value-pair-arguments
+def _attributes_to_dict(
+    ctx: click.Context, attribute: click.Option, attributes: tuple[str, ...] | None
+) -> dict[str, str]:
+    """Click callback that converts attributes specified in the form `key=value` to a
+    dictionary"""
+    result = {}
+    if attributes is not None:
+        for arg in attributes:
+            k, v = arg.split("=")
+            if k in result:
+                raise click.BadParameter(f"Attribute {k!r} is specified twice")
+            result[k] = v
+
+    return result
 
 
 @click.command()
@@ -13,12 +30,22 @@ ZARR_MAJOR_VERSION = zarr.__version__.split(".")[0]
 @click.option("-v", "--variable", type=str, help="Dump variable's info")
 @click.option("-m", "--max-rows", default=999, help="Maximum number of rows to display")
 @click.option("-i", "--info", is_flag=True, help="Use ncdump style")
-def dump(url: str, variable: str, max_rows: int, info: bool):
-    fs, _, _ = fsspec.get_fs_token_paths(url)
+@click.option(
+    "--storage-option",
+    help="Key/value pair separated by '=', to be passed to the storage_options "
+         "argument of fsspec. It can be used multiple times to pass multime arguments. "
+         "example: --storage-option profile=contabo "
+         "--storage-option endpoint=https://eu2.contabostorage.com",
+    multiple=True,
+    callback=_attributes_to_dict,
+    default=None
+)
+def dump(url: str, variable: str, max_rows: int, info: bool, storage_option: dict):
+    fs, _, _ = fsspec.get_fs_token_paths(url, storage_options=storage_option)
     if not fs.exists(url):
         raise click.ClickException(f"No file or directory at {url}")
 
-    object_, object_is_xarray = _open_with_xarray_or_zarr(url)
+    object_, object_is_xarray = _open_with_xarray_or_zarr(url, storage_option)
 
     if variable is not None:
         if info:
@@ -36,7 +63,7 @@ def dump(url: str, variable: str, max_rows: int, info: bool):
 
 
 def _open_with_xarray_or_zarr(
-    url: str,
+    url: str, storage_option: dict
 ) -> Tuple[Union[xr.Dataset, zarr.Group, zarr.Array], bool]:
     if ZARR_MAJOR_VERSION >= "3":
         # TODO: remove ValueError here once a version of xarray is released
@@ -46,11 +73,11 @@ def _open_with_xarray_or_zarr(
         exceptions = (KeyError, TypeError)
 
     try:
-        result = xr.open_zarr(url)
+        result = xr.open_zarr(url, storage_options=storage_option)
         is_xarray_dataset = True
     except exceptions:
         # xarray cannot open dataset, fall back to using zarr directly
-        result = zarr.open(url)
+        result = zarr.open(url, storage_options=storage_option)
         is_xarray_dataset = False
 
     return result, is_xarray_dataset
